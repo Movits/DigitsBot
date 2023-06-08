2dcc2b7a01a53d1329b53422b361590fd60510e2
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-08 00:30:51 -0300

    reverting some changes because im changing hosting service

--- DigitsBot.py
+++ DigitsBot.py
@@ -9,26 +9,11 @@ from discord.utils import get # Import the 'get' utility from Discord.py
 from discord import FFmpegPCMAudio # Import the FFmpegPCMAudio class for playing audio
 from yt_dlp import YoutubeDL # Import the YoutubeDL class from yt-dlp
 from dotenv import load_dotenv
-import aiohttp
 
 # Load tokens and API keys from environment variables
 load_dotenv()
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
-
-proxy_url = 'http://proxy.server:3128'
-
-class MyBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    async def start(self, *args, **kwargs):
-        # This line creates an aiohttp session within an async context
-        self.http_session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), trust_env=True)
-        await super().start(*args, **kwargs)
-
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
 
 # Set up the bot with default intents and the desired command prefix
 intents = discord.Intents.default()
