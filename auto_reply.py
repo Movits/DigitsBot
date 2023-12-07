80652ece3c97943010ca2665af362871674eac77
Movits <robertomovits@gmail.com>
2023-12-07 19:29:49 -0300

    created and added connection to DB to save the user responses

--- auto_reply.py
+++ auto_reply.py
@@ -1,24 +1,48 @@
+import os
 from discord.ext import commands
+import mysql.connector
+from dotenv import load_dotenv
+import asyncio
+
+# Load environment variables from .env file
+load_dotenv()
 
 class AutoReply(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.user_responses = {}  # Store user responses
+        # Establish a database connection using the environment variables
+        self.db = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
+        )
+        self.cursor = self.db.cursor(buffered=True)
 
     @commands.command(name='addresponse')
     async def add_response(self, ctx, *, response):
-        self.user_responses[ctx.author.id] = response
+        user_id = str(ctx.author.id)
+        sql = "REPLACE INTO responses (user_id, response) VALUES (%s, %s)"
+        val = (user_id, response)
+        # Execute SQL command in an executor to avoid blocking
+        await self.bot.loop.run_in_executor(None, self.cursor.execute, sql, val)
+        self.db.commit()
         await ctx.send(f"Response set to: {response}")
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot:
+        if message.author.bot or message.author == self.bot.user:
             return
 
-        mentions = [mention for mention in message.mentions if mention.id in self.user_responses]
+        mentions = [mention for mention in message.mentions if mention.id != self.bot.user.id]
         for mention in mentions:
-            response = self.user_responses[mention.id]
-            await message.channel.send(response)
+            sql = "SELECT response FROM responses WHERE user_id = %s"
+            val = (str(mention.id),)
+            # Execute SQL command in an executor to avoid blocking
+            await self.bot.loop.run_in_executor(None, self.cursor.execute, sql, val)
+            result = self.cursor.fetchone()
+            if result:
+                await message.channel.send(result[0])
 
 async def setup(bot):
     await bot.add_cog(AutoReply(bot))
